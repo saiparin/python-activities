95bf6d74117a9bb15a606f734c04bdaf27428191
saiparin <saiparin2011@gmail.com>
2026-08-01 19:07:46 +0200

    Add files via upload

---
+++ class3act2.py
@@ -0,0 +1,14 @@
+name= "sai"
+c=7.5
+age=15
+d= False
+
+print("name:",name, type(name))
+print("C:",c, type(c))
+print("age:",age, type(age))
+print("d:",d, type(d))
+
+age= str(age)
+print("age:",age,type(age))
+c= int(c)
+print("c:",c, type(c))
