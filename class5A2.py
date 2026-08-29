06edc654c86a860221baf66b97bd3d9e232a8be1
saiparin <saiparin2011@gmail.com>
2026-08-29 18:15:11 +0200

    Add files via upload

---
+++ class5A2.py
@@ -0,0 +1,8 @@
+actual_cost= float(input("enter the actual cost:"))
+sale_cost=float(input("enter the sale cost:"))
+
+if (sale_cost>actual_cost):
+    print("made a profit")
+
+else:
+    print("made a loss")
