4789f9b79036ecfe5f912cbc31236c2ac5999dca
saiparin <saiparin2011@gmail.com>
2026-08-29 12:57:31 +0200

    Add files via upload

---
+++ class4A3.py
@@ -0,0 +1,14 @@
+
+print("Enter Marks Obtained in 4 Subjects: ")
+math = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+german = int(input("german :"))
+
+
+sum = math+english+science+german
+print("sum of math,english,science and german = ",sum)
+
+perc = (sum/400)*100
+
+print(end="Percentage Mark = ")
