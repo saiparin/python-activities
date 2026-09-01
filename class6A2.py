4ecf8fe5524ee30fe72fdffb534e40559df2b3ca
saiparin <saiparin2011@gmail.com>
2026-09-01 17:41:32 +0200

    Add files via upload

---
+++ class6A2.py
@@ -0,0 +1,22 @@
+a=10
+b=27
+c=27
+
+print((not a==b))
+print((not b==c))
+
+
+a="hello"
+b="weather"
+
+if not (a==b):
+    print(" they are not the same")
+
+a=6
+b=9
+
+if not((a==1)==(b==5)):
+    print("hello")
+a = int(input("enter a number:"))
+if not(a%2==0):
+    print("the number is odd")
