0295194413a09f8bf280dc412e65c9720d4b27e7
saiparin <saiparin2011@gmail.com>
2026-09-01 17:28:19 +0200

    Add files via upload

---
+++ class6A1.py
@@ -0,0 +1,21 @@
+a=17
+b=6
+c=0
+
+if a and b and c:
+    print(" all the numbers are above zero")
+else:
+    print("one or more number is zero or below zero")
+
+a=14
+b=-7
+c=0 
+
+if a>0 or b>0 :
+    print(" one of the number is above zero")
+else:
+    print("both the numbers are above zer or below zero")
+if b>0 or c>0:
+    print(" one of the number is above zero")
+else:
+    print("both the  numbers are above zer or below zero")
