f179df110068f1a65e0cab81a71fdd85661c6bb2
saiparin <saiparin2011@gmail.com>
2026-09-09 15:07:39 +0200

    Add files via upload

---
+++ age.py
@@ -0,0 +1,7 @@
+age_student = input("Please enter your age: ")
+age = int(age_student) 
+
+if age >= 10 and age <= 20:
+    print("good luck with finishing your studies!")
+if age >= 21 and age <= 30:
+    print("good luck with your career!")
